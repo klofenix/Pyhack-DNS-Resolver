4685dddbe239d4c48bbcd7485e3f1a2180cf7afc
Klofenix | Camilo Martínez <77837384+klofenix@users.noreply.github.com>
2022-01-05 21:42:38 +0100

    Update pyhack-dns-resolver.py

--- pyhack-dns-resolver.py
+++ pyhack-dns-resolver.py
@@ -4,7 +4,7 @@ def main():
     try:
       objetivo = dns.resolver.query("XXXX.com", "NS")
       for x in objetivo:
-        print(x)
+        print("[+]"+ x)
     except:
       print("***ERROR DETECTED*** No se puede obtener información")
 if __name__== '__main':
